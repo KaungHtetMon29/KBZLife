129b202886db342145aabd3428845ad656e2659b
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2024-03-02 13:18:19 +0630

    fix: select bug and added style

--- src/component/Submenu.tsx
+++ src/component/Submenu.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 import SideBig from "../assets/SideBig";
 import { useSelector } from "react-redux";
 import { RootState } from "../utils/redux/store";
@@ -7,6 +7,7 @@ import {
   MenuInterface,
   SubMenuInterface,
 } from "../utils/Data";
+import { Dispatch } from "@reduxjs/toolkit";
 
 export default function Submenu() {
   // const [show, setShow] = React.useState<MenuInterface>();
@@ -14,13 +15,14 @@ export default function Submenu() {
   const menustate = useSelector((state: RootState) => state.menu.menustate);
   const show = useSelector((state: RootState) => state.menu.selectedItem);
   const rotation = [60, 180, 300];
+  const [HoverItem, setHoveredItem] = useState(null);
   useEffect(() => {
-    console.log(show);
-  }, [show]);
+    console.log(HoverItem);
+  }, [HoverItem]);
   return (
-    <div className="absolute items-center flex justify-center">
+    <div className="absolute items-center flex justify-center group">
       {show.itemmenu !== null && show.itemmenu !== undefined && (
-        <SideBig item={show.itemmenu} />
+        <SideBig item={show.itemmenu} HoverItem={setHoveredItem} />
       )}
 
       {Object.keys(show).length !== 0 &&
@@ -28,13 +30,15 @@ export default function Submenu() {
         show.itemmenu.map((i: ItemMenuInterface, index: number) => (
           <div
             style={{ transform: `rotate(${rotation[index]}deg)` }}
-            className={`absolute origin-bottom  w-full`}
+            className={`absolute origin-bottom w-full `}
           >
             <div
               className={`absolute transform origin-bottom items-center flex justify-center translate-x-[320px] w-full`}
             >
               <h2
-                className={`absolute text-base font-semibold w-full rotate-[270deg] transform scale-x-[1] text-center`}
+                className={`absolute text-base font-semibold w-full rotate-[270deg] transform scale-x-[1] text-center ${
+                  HoverItem === index ? "text-white" : "text-black"
+                }`}
               >
                 {i.item}
               </h2>
